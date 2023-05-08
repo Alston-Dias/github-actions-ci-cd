114d05ff386bc5448969694a0c85095343dfc955
Alston Dias <adias@redhat.com>
2023-05-08 11:22:29 +0000

    fix(release.config.js): updated GitHub URL

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     branches: "master",
-    repositoryUrl: "https://github.com/AlstonDias/github-action-ci-cd/",
+    repositoryUrl: "https://github.com/Alston-Dias/github-actions-ci-cd",
     plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/npm', ['@semantic-release/github', {
         assets: [
             { path: "build.zip", label: "Build" },
